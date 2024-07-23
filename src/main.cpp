32f4cd406aeee0d4314cc1e052f1a33ca3fdb488
Francis Olivier Houle <francis.o.houle@outlook.com>
2024-07-23 13:41:13 -0400

    Changed the whole main.ccp code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,42 @@
+// Library Include List
 #include <Arduino.h>
 #include <Stepper.h>
 
-int powerState = 0;                   // ON OFF tracking
-int changePowerState = 0;             // Variable of the power button
-int rolePerMinute = 7;               // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
-const int powerPin = 2;               // ON OFF Switch
-const int stepPerRevolution = 4096;
+// Variables déclaration
+const int stepPerRevolution = 2048;        // # of Step for Full 360 rotation in Full Step mode. Change value for 4096 for half-step mode
+const int switchPin = 2;                   // ON OFF Switch
+bool activationState = LOW;                // Track if the motor is running or not.
+int switchPress = LOW;                     // Track if the button is press
 
-Stepper myStepper = Stepper(stepPerRevolution, 8, 9, 10, 11);
+Stepper platformMotor = Stepper(stepPerRevolution, 8, 9, 10, 11);   //function Stepper(steps, pin1, pin2, pin3, pin4)
 
 void setup() {
-  pinMode(powerPin, INPUT);
-  Serial.begin(9600);
-} 
+//Initialization of the configuration on boot
+  pinMode(switchPin, INPUT);               // Set "read" mode on the activation button
+  platformMotor.setSpeed(0);               // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
+  Serial.begin(9600);                      // Begin Serial communication at a baud rate of 9600:
+  Serial.println("The program is starting");                  
+}
 
 void loop() {
-  changePowerState = digitalRead(powerPin);
+// Program running in loop
+  switchPress = digitalRead(switchPin);   // Read the switch PIN to know if it has been press
 
-  if (changePowerState == HIGH) {
-    Serial.println(changePowerState + powerState);
-    if (powerState = 0) {
-      motorStart();
-    }
-    else {
-      motorStop();
-    }
-  }
-  else {
-    Serial.println(changePowerState + powerState);
+  if (switchPress == HIGH) {
+    activationState = !activationState;
   }
 
-  delay(250);
+  if (activationState == LOW) {
+    platformMotor.setSpeed(0);            // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
+  } else {
+    platformMotor.setSpeed(5);            // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
+  }
+  
+  platformMotor.step(stepPerRevolution);  // Turn the motor in  clockwise for a full rotation
+  delay(500);
+  
+  platformMotor.step(-stepPerRevolution); // Turn the motor in counterclockwise for a full rotation
+  delay(500);
 }
 
-// put function definitions here:
-int motorStart() {
-  powerState = 1;
-  myStepper.setSpeed(rolePerMinute);
-}
-
-int motorStop() {
-  powerState = 0;
-  myStepper.setSpeed(0);
-}
+// put function definitions here
