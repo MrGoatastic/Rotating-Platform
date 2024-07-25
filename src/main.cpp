241cc23b10973a9e5e230ac4c32040da477668fd
Francis Olivier Houle <francis.o.houle@outlook.com>
2024-07-25 16:48:46 -0400

    Added debouncing to rotate the stepper

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,52 @@ const int stepPerRevolution = 2048;        // # of Step for Full 360 rotation in
 const int switchPin = 2;                   // ON OFF Switch
 bool activationState = LOW;                // Track if the motor is running or not.
 int switchPress = LOW;                     // Track if the button is press
+int lastSwitchReading = LOW;                 // Record last reading of the switchPin
+unsigned long lastDebounceTime = 0;        
+unsigned long debounceDelay = 50;
 
 Stepper platformMotor = Stepper(stepPerRevolution, 8, 9, 10, 11);   //function Stepper(steps, pin1, pin2, pin3, pin4)
 
-void setup() {
 //Initialization of the configuration on boot
+void setup() {
   pinMode(switchPin, INPUT);               // Set "read" mode on the activation button
-  platformMotor.setSpeed(0);               // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
+  platformMotor.setSpeed(5);               // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
   Serial.begin(9600);                      // Begin Serial communication at a baud rate of 9600:
   Serial.println("The program is starting");                  
 }
 
-void loop() {
 // Program running in loop
-  switchPress = digitalRead(switchPin);   // Read the switch PIN to know if it has been press
+void loop() {
+  // Read the switch PIN to know if it has been press
+  int reading = digitalRead(switchPin);   
 
-  if (switchPress == HIGH) {
-    activationState = !activationState;
+  // Make sure switch has been press by debouncing
+  if (reading != lastSwitchReading) {
+    lastDebounceTime = millis();
   }
 
-  if (activationState == LOW) {
-    platformMotor.setSpeed(0);            // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
-  } else {
-    platformMotor.setSpeed(5);            // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
+  // Change activationState
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != switchPress) {
+      switchPress = reading;
+      if (switchPress == HIGH) {
+        activationState = !activationState;
+        Serial.println("Switch pressed");
+      }
+    }
   }
+
+  // Change the Record of the last reading for next debouncing
+  lastSwitchReading = reading;
   
-  platformMotor.step(stepPerRevolution);  // Turn the motor in  clockwise for a full rotation
-  delay(500);
-  
-  platformMotor.step(-stepPerRevolution); // Turn the motor in counterclockwise for a full rotation
-  delay(500);
+  // Rotate the Platform by looking at the tracker 
+  if (activationState == HIGH) {
+    Serial.println("Rotating the platform");
+    platformMotor.step(stepPerRevolution);
+    delay(500);
+    platformMotor.step(-stepPerRevolution);
+    delay(500);
+  }
 }
 
-// put function definitions here
+// Put function definitions here
