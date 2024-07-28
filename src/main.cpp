2c68532effba5489be13b7c5d1f651e7ccf2d2f7
Francis Olivier Houle <francis.o.houle@outlook.com>
2024-07-28 10:12:31 -0400

    MCU addition

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
-// Variables déclaration
+// Variables declaration
 const int stepPerRevolution = 2048;        // # of Step for Full 360 rotation in Full Step mode. Change value for 4096 for half-step mode
 const int switchPin = 2;                   // ON OFF Switch
 bool activationState = LOW;                // Track if the motor is running or not.
@@ -16,7 +16,7 @@ Stepper platformMotor = Stepper(stepPerRevolution, 8, 9, 10, 11);   //function S
 //Initialization of the configuration on boot
 void setup() {
   pinMode(switchPin, INPUT);               // Set "read" mode on the activation button
-  platformMotor.setSpeed(5);               // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
+  platformMotor.setSpeed(1);               // Set the Speed of the stepper (The maximum speed for a 28byj-48 stepper motor is roughly 10-15 rpm at 5 V.)
   Serial.begin(9600);                      // Begin Serial communication at a baud rate of 9600:
   Serial.println("The program is starting");                  
 }
@@ -38,6 +38,7 @@ void loop() {
       if (switchPress == HIGH) {
         activationState = !activationState;
         Serial.println("Switch pressed");
+        delay(1000);
       }
     }
   }
@@ -48,10 +49,9 @@ void loop() {
   // Rotate the Platform by looking at the tracker 
   if (activationState == HIGH) {
     Serial.println("Rotating the platform");
-    platformMotor.step(stepPerRevolution);
-    delay(500);
-    platformMotor.step(-stepPerRevolution);
-    delay(500);
+    platformMotor.step(10);
+  } else {
+    Serial.println("Motor stopped");
   }
 }
 
